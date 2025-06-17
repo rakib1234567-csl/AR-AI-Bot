2ab3d1a259ed54fc537ee702139e5a24b78fbf67
rakib1234567-csl <rakib.ali.csl@gmail.com>
2025-06-17 23:53:58 +0600

    Update handleEvent.js

--- Nayan/system/handle/handleEvent.js
+++ Nayan/system/handle/handleEvent.js
@@ -3,7 +3,7 @@ module.exports = function ({api ,models, Users, Threads, Currencies }) {
     const moment = require("moment");
     return function ({ event }) {
         const timeStart = Date.now()
-        const time = moment.tz("Asia/Manila").format("HH:MM:ss L");
+        const time = moment.tz("Asia/Dhaka").format("HH:MM:ss L");
         const { userBanned, threadBanned } = global.data;
         const { events } = global.client;
         const { allowInbox } = global.Nayan;
